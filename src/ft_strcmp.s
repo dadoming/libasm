56040d0487963fde4d7908e7e9df30c509c2a4a1
dadoming <davidmdg98@hotmail.com>
2024-04-26 23:12:14 +0100

    ready eval

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -10,26 +10,26 @@ ft_strcmp:
 ;   rax: 0 if strings are equal, positive if first string is greater, negative if second string is greater
 
   loop:
-	mov al, [rdi]
-	mov bl, [rsi]
-	cmp al, bl
+		mov al, [rdi]
+		mov bl, [rsi]
+		cmp al, bl
 
-	cmp al, 0
-	je done
-	cmp bl, 0
-	je done
+		cmp al, 0
+		je done
+		cmp bl, 0
+		je done
 
-	cmp al, bl
-	jne done
+		cmp al, bl
+		jne done
 
-	inc rdi
-	inc rsi
-	jmp loop
+		inc rdi
+		inc rsi
+		jmp loop
 
   done:
 	; movzx makes sure that the value is zero extended, meaning that the upper bits are set to zero
 	; while mov just copies the value as is and the upper bits are not cleared (set to 0)
-	movzx rax, al
-	movzx rbx, bl
-	sub rax, rbx
-	ret
+		movzx rax, al
+		movzx rbx, bl
+		sub rax, rbx
+		ret
--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -10,11 +10,12 @@ ft_strlen:
 
   mov		rax, 0		; set rax to 0
   jmp		loop			; jump to loop
+
   loop:
-  cmp		BYTE [rdi + rax], 0	; check null byte
-  je		done	              ; je is jump if equal which means if null byte is found jump to done
-  inc		rax	                ; increment rax by one
-  jmp     loop				      ; jump back to loop
+    cmp		BYTE [rdi + rax], 0	; check null byte
+    je		done	              ; je is jump if equal which means if null byte is found jump to done
+    inc		rax	                ; increment rax by one
+    jmp   loop				      ; jump back to loop
 
   done:
     ret
